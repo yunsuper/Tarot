30b1f5026fe1b3b4291e96b0e645d9bbf417fdc1
yunsuper <yunsuper80@gmail.com>
2026-01-17 16:14:35 +0900

    feat: Vercel Analytics 도입 및 방문자 분석 기능 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
     <App />
